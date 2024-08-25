8c4e7b08cb7ef113ac6c7c7b12523657e34d0fc4
heisdanielade <heisdanielade@gmail.com>
2024-08-25 19:44:43 +0200

    added scroll reveal animation

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+
+
+
+// Display current year in footer
+const currentYear = new Date().getFullYear();
+document.getElementById('year').textContent = currentYear;
+
+
+//Scroll reveal animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('shown');
+        } else {
+            entry.target.classList.remove('shown');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.not-shown');
+hiddenElements.forEach((el) => observer.observe(el));
