4991ebe0a9148b8222325d94f13ec35edcb1469d
heisdanielade <heisdanielade@gmail.com>
2025-01-05 23:29:05 +0100

    added page loader opacity change (javascript)

--- public/js/main.js
+++ public/js/main.js
@@ -57,7 +57,7 @@ const greetings = [
   setInterval(changeGreeting, 700);
   
   setTimeout(() => {
-    // document.getElementById("page-loader").style.opacity = "0";
+    document.getElementById("page-loader").style.opacity = "0";
     setTimeout(() => {
       document.getElementById("page-loader").style.display = "none";
     }, 600);
